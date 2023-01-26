33331ea0ba4fdbd074e7cb138fc68d95d575dd5e
erenergene <erenergene@crc-dot1x-nat-10-239-136-211.bu.edu>
2023-01-26 15:22:48 -0500

    var name changes

--- BulkSim.m
+++ BulkSim.m
@@ -33,11 +33,11 @@ BulkL = linspace(100,120,201); % Thickness from 100 nm to 120 nm
 %% CHANGE N_SIO2 N_SI
 
 for i = 1
-for j = 1
-for k = 1:numel(Bulkn)
-[Refmat_lamR_L0_n_1(i,j,k),Z_lamR_L0_n_1(i,j,k)] = multidiel1([Bulkn(k);n_SiO2(find(lambda==cw_r),2);n_Si(find(lambda==cw_r),2)],0,cw_r); 
-end
-end
+    for j = 1
+        for k = 1:numel(Bulkn)
+        [Refmat_lamR_L0_n_1(i,j,k),Z_lamR_L0_n_1(i,j,k)] = multidiel1([Bulkn(k);n_SiO2(find(lambda==cw_r),2);n_Si(find(lambda==cw_r),2)],0,cw_r); 
+        end
+    end
 end
 %% Calculates reflectance at Silicon (L = 0) as a function of refractive index (n from 1.33 to 1.35)
 
@@ -57,6 +57,7 @@ RefmatSi_bulk = conj(Refmat1Si_bulk).*Refmat1Si_bulk;
 Refmat_lam_L_n_1 = []
 Z1_lam_L_n_1 = []
 
+tic
 
 for i = 1:numel(lambda)
     for j = 1:numel(BulkL)
@@ -66,30 +67,34 @@ for i = 1:numel(lambda)
         end
     end
 end
+
+toc
+
+
 %%
 Refmat_lam_L_n = conj(Refmat_lam_L_n_1).*Refmat_lam_L_n_1;
 
 %% Get reflectance vals from n = 1.33 to 1.35 and thickness 100 120 nm for red led
 
-% tic
-% 
-% for i = 1:numel(Bulkn)
-% for j = 1:numel(BulkL)
-% fprintf("Now running ref index %.0f\n",i)
-% fprintf("Now running thickness %.0f\n",j)
-% 
-% [Refmat1_bulk(i,j),Z1(i,j)] = multidiel1([Bulkn(i);n_SiO2(find(lambda==cw_r),2);n_Si(find(lambda==cw_r),2)],BulkL(j).*n_SiO2(find(lambda==cw_r),2),cw_r);        
-% 
-% % Calculates reflectance as a function of thickness L from 100 to 120 nm and refractive index n from 1.33 to 1.35 
-% 
-% end
-% end
-% 
-% toc
-% 
-% %%
-% 
-% Refmat_bulk = conj(Refmat1_bulk).*Refmat1_bulk; %Refmat_Bulk(n,L) 
+tic
+
+for i = 1:numel(Bulkn)
+for j = 1:numel(BulkL)
+fprintf("Now running ref index %.0f\n",i)
+fprintf("Now running thickness %.0f\n",j)
+
+[Refmat1_bulk(i,j),Z1(i,j)] = multidiel1([Bulkn(i);n_SiO2(find(lambda==cw_r),2);n_Si(find(lambda==cw_r),2)],BulkL(j).*n_SiO2(find(lambda==cw_r),2),cw_r);        
+
+% Calculates reflectance as a function of thickness L from 100 to 120 nm and refractive index n from 1.33 to 1.35 
+
+end
+end
+
+toc
+
+%%
+
+Refmat_bulk = conj(Refmat1_bulk).*Refmat1_bulk; %Refmat_Bulk(n,L) 
 
 %% Display reflectance for 5 different thickness from 100 to 120 nm for single wavelength lambda = 633 nm
 
@@ -127,34 +132,62 @@ saveas(figure(2),[pwd '/Figures/BulkSim/2Refvs5L.fig']);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% GET THESE ACCORDING TO CHANGED VARIABLE NAMES AND FORMAT %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-RefmatSi_bulk = RefmatSi_bulk';
-subbulk = RefmatSi_bulk - Refmat_bulk; % Subtract T1 oxide reflectance from Silicon reflectance
-addbulk = RefmatSi_bulk + Refmat_bulk; % Add T1 oxide reflectance and Silicon reflectance
-ratbulk = subbulk ./ addbulk; % Ratio
+%%
+Refmat_lamR_L0_n = reshape(Refmat_lamR_L0_n,[21 1]); % Reflectance at silicon for n from 1.33 to 1.35
+%%
+Refmat_lamR_L_n = reshape(Refmat_lam_L_n(find(lambda == cw_r),:,:),[201 21])';
+%%
+subtrSiT1 = Refmat_lamR_L0_n - Refmat_lamR_L_n;
+addSiT1 = Refmat_lamR_L0_n + Refmat_lamR_L_n;
+ratSiT1 = subtrSiT1 ./ addSiT1;
+%%
+
+%%
+% RefmatSi_bulk = RefmatSi_bulk';
+% subbulk = RefmatSi_bulk - Refmat_bulk; % Subtract T1 oxide reflectance from Silicon reflectance
+% addbulk = RefmatSi_bulk + Refmat_bulk; % Add T1 oxide reflectance and Silicon reflectance
+% ratbulk = subbulk ./ addbulk; % Ratio
 %% 
 
 
 
 %% Display ratio R_Si - R_T1 / R_Si + R_T1 as a function of n from 1.33 to 1.35
 
+% figure(30) 
+% 
+% hold on
+% plot(Bulkn,ratbulk(:,find(BulkL==100)),'LineWidth',2)
+% plot(Bulkn,ratbulk(:,find(BulkL==105)),'LineWidth',2)
+% plot(Bulkn,ratbulk(:,find(BulkL==110)),'LineWidth',2)
+% plot(Bulkn,ratbulk(:,find(BulkL==115)),'LineWidth',2)
+% plot(Bulkn,ratbulk(:,find(BulkL==120)),'LineWidth',2)
+% legend('L=100','L=105','L=110','L=115','L=120')
+% xlabel('ref index');ylabel('Ratio')
+% xlim([1.33 1.35])
+% title ('(R_S_i - R_T_1) / (R_S_i + R_T_1)')
+% saveas(figure(3),[pwd '/Figures/BulkSim/3RatBulk.fig']);
+
+%%
+
 figure(3) 
 
 hold on
-plot(Bulkn,ratbulk(:,find(BulkL==100)),'LineWidth',2)
-plot(Bulkn,ratbulk(:,find(BulkL==105)),'LineWidth',2)
-plot(Bulkn,ratbulk(:,find(BulkL==110)),'LineWidth',2)
-plot(Bulkn,ratbulk(:,find(BulkL==115)),'LineWidth',2)
-plot(Bulkn,ratbulk(:,find(BulkL==120)),'LineWidth',2)
+plot(Bulkn,ratSiT1(:,find(BulkL==100)),'LineWidth',2)
+plot(Bulkn,ratSiT1(:,find(BulkL==105)),'LineWidth',2)
+plot(Bulkn,ratSiT1(:,find(BulkL==110)),'LineWidth',2)
+plot(Bulkn,ratSiT1(:,find(BulkL==115)),'LineWidth',2)
+plot(Bulkn,ratSiT1(:,find(BulkL==120)),'LineWidth',2)
 legend('L=100','L=105','L=110','L=115','L=120')
 xlabel('ref index');ylabel('Ratio')
 xlim([1.33 1.35])
 title ('(R_S_i - R_T_1) / (R_S_i + R_T_1)')
 saveas(figure(3),[pwd '/Figures/BulkSim/3RatBulk.fig']);
 
+
 %% Get and display slope of ratio for L from 100 to 120 nm
 
 for i = 1:numel(BulkL)
-    slprat(i) = (ratbulk(end,i) - ratbulk(1,i)) / (1.35-1.33); % Calculates slope of ratio R_Si - R_T1 / R_Si + R_T1
+    slprat(i) = (ratSiT1(end,i) - ratSiT1(1,i)) / (1.35-1.33); % Calculates slope of ratio R_Si - R_T1 / R_Si + R_T1
 end
 
 
@@ -170,6 +203,8 @@ title ('Slope of ratio (R_S_i - R_T_1) / (R_S_i + R_T_1)')
 saveas(figure(4),[pwd '/Figures/BulkSim/4SlpRat.fig']);
 
 %% Code to test n in a broader range (1 to 3)
+
+%% BURAYA KADAR OK: BURADAN SONRA CHANGE VAR NAMES
 
 ntest = linspace(1,3,201); 
 
